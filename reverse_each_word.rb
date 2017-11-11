2f31407ba57f29bac655c6361efe0af81b8cc3cf
David Welch <dmw9400@gmail.com>
2017-11-11 15:43:22 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,10 @@
+def reverse_each_word(string)
+  new_array = string.split(" ")
+  last_item = new_array[-1]
+  new_array.pop
+  new_string = ""
+  new_array.each do |word|
+    new_string = new_string + "#{word.reverse!} "
+  end
+  return new_string + last_item.reverse!
+end
