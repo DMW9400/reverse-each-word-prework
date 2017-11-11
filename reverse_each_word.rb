7b1c16897ac946e2ce0ad0f4e564340bed1d2e5e
David Welch <dmw9400@gmail.com>
2017-11-11 15:43:37 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -3,7 +3,7 @@ def reverse_each_word(string)
   last_item = new_array[-1]
   new_array.pop
   new_string = ""
-  new_array.each do |word|
+  new_array.collect do |word|
     new_string = new_string + "#{word.reverse!} "
   end
   return new_string + last_item.reverse!
